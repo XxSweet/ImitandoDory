3fc21e94cd7a239e18516bebb487281c5a2ae848
xxsweet <xxsweet07@gmail.com>
2023-03-31 20:33:44 -0300

    exercicio imitando dory

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("--- Imitando Dory ---");
+
+Console.Write("O que voce quer falar para amiga baleia...: ");
+string frase = Console.ReadLine()!;
+
+string EmBaleies = frase
+    .Replace("a", "aaa")
+    .Replace("A", "AAA")
+    .Replace("e", "eee")
+    .Replace("E", "EEE")
+    .Replace("i", "iii")
+    .Replace("I", "III")
+    .Replace("O", "OOO")
+    .Replace("u", "uuu")
+    .Replace("U", "UUU")
+;
+
+Console.WriteLine($"\nEm baleiês:\n\n{EmBaleies}");
